099d6a1367fde9de4fc2e234f48e3d099a393b2c
Lechner Julian - FrauJulian <contact@fraujulian.xyz>
2025-07-07 17:35:46 +0200

    Delete

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export {AudioManager} from "./Services/AudioManager";
-export type {VoiceConnectionDataModel} from "./Models/VoiceConnectionDataModel";
-export type {VoiceAudioDataModel} from "./Models/VoiceAudioDataModel";
