9bbc78da27ad693c89874e48e008f1bf9a775ce7
Lechner Julian - FrauJulian <contact@fraujulian.xyz>
2025-07-28 17:58:15 +0200

    improve type accuracy;

--- src/audioManager.ts
+++ src/audioManager.ts
@@ -10,7 +10,7 @@ import {join} from "node:path";
 import {VoiceConnectionDataModel} from "./types/VoiceConnectionDataModel";
 import {VoiceAudioDataModel} from "./types/VoiceAudioDataModel";
 
-export default class AudioManager {
+declare class AudioManager {
     public VoiceConnection: VoiceConnection | null = null;
     public AudioPlayer: AudioPlayer | null = null;
     public AudioResource: AudioResource | null = null;
